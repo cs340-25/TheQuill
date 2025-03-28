4e3aa7601df766ddf06c28bc58f9a7d7d0b5c8ec
yzabxelle <ayq.quijano@gmail.com>
2025-03-27 21:10:52 -0400

    setup firebaseconfig

---
+++ src/firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDgHtzdeZcDKzf_jH6eKBRkap-WkzwuHh8",
+  authDomain: "the-quill-1f02d.firebaseapp.com",
+  projectId: "the-quill-1f02d",
+  storageBucket: "the-quill-1f02d.firebasestorage.app",
+  messagingSenderId: "360830342665",
+  appId: "1:360830342665:web:3dc561cd073545afaf8ad8",
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
