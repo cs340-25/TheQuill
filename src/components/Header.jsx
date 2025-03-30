f788d4baa819e9a30fc6bf5d72ccfa6953a8fcf8
yzabxelle <ayq.quijano@gmail.com>
2025-03-30 18:13:27 -0400

    fixed some small syntax issues

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import BookSearch from "./BookSearch"
 
-function Header(){
-   return(
-    <div>
-      <h1>Welcome to the Book Finder App</h1>
-      <BookSearch />
-    </div>
-   ); 
+function Header() {
+    return (
+        <div>
+            <h1>Welcome to the Book Finder App</h1>
+            <BookSearch />
+        </div>
+    );
 }
 export default Header
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -3,23 +3,23 @@ import { Link } from "react-router-dom"
 
 function NavBar() {
 
-    return(
+    return (
         <nav className="navbar">
 
             <div className="navbar-name">
-            GreatReads
+                GreatReads
             </div>
 
             <div className="navbar-search">
                 <BookSearch />
             </div>
-            
+
             <ul className="navbar-links">
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/my-books">My Books</Link></li>
                 <li><Link to="/ friends">friends</Link></li>
             </ul>
-            
+
         </nav>
 
     );
