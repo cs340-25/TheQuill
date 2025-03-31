f844248f2f10b98156e273a6dca053181301ef9e
therobat1 <silviaelkomos2004@gmail.com>
2025-03-31 19:43:38 -0400

    nav bar and search page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import BookDetails from "./pages/BookDetails";
 import UserProfile from "./pages/UserProfile";
-<<<<<<< Updated upstream
-=======
 import NavigationBar from "./components/NavigationBar";
 import BookSearch from "./components/BookSearch";
 import SearchPage from "./pages/SearchPage";
->>>>>>> Stashed changes
 
 function App() {
   return (
@@ -16,12 +13,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/book/:id" element={<BookDetails />} />
         <Route path="/user/:userName" element={<UserProfile />} />
-<<<<<<< Updated upstream
-=======
         <Route path="/book" element={<BookSearch />} />
         <Route path="/nav" element={<NavigationBar />} />
         <Route path="/search" element={<SearchPage />} />
->>>>>>> Stashed changes
       </Routes>
     </Router>
   );
